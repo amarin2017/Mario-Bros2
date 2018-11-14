01a58f6def292726fa4ad1540754ac82113adf6d
Adriana Marin <adriana_marin1@baylor.edu>
2018-11-14 17:22:21 -0600

    Initial commit

---
+++ image_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "image_test.h"
+#include <SDL2/SDL.h>
+
+SDL_Surface* getMario(SDL_Surface *imageSurface)
+{
+    imageSurface = SDL_LoadBMP("mario.bmp");
+
+}
+
+//
+// Created by MARIN on 11/14/2018.
+//
+
---
+++ image_test.h
@@ -0,0 +1,16 @@
+//
+// Created by MARIN on 11/14/2018.
+//
+
+#ifndef MARIO_BROS2_IMAGE_TEST_H
+#define MARIO_BROS2_IMAGE_TEST_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+using namespace std;
+
+SDL_Surface* getMario(SDL_Surface);
+
+#endif //MARIO_BROS2_IMAGE_TEST_H
